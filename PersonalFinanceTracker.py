116d85b503196031b74e70e2374bfe2bed254adb
John Grider <jdgride@users.noreply.github.com>
2020-12-06 15:03:08 -0500

    Create PersonalFinanceTracker.py
    Initial Commit

---
+++ PersonalFinanceTracker.py
@@ -0,0 +1,5 @@
+# Authors: John Grider and Chris Rovero
+# Date: 12/6/2020
+# Purpose: Read and write data to and from an excel file to create visually appealing displays related to personal
+# finance.
+
