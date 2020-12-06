df87d3ea1e1221f199888b8f2778392e6b15b9cc
John Grider <35738039+jdgride@users.noreply.github.com>
2020-12-06 15:15:43 -0500

    Update PersonalFinanceTracker.py

--- PersonalFinanceTracker.py
+++ PersonalFinanceTracker.py
@@ -3,4 +3,7 @@
 # Purpose: Read and write data to and from an excel file to create visually appealing displays related to personal
 # finance.
 
-#Test
+# List of Rovero tasks: 
+# 1. Check for existing excel file in folder, if none exists create one.
+# 2. Open the file, write some test data to it, and close it
+# 3. Open the file, and read the data from it.
